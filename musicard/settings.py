d5f3f8d3d9340562b7a2da605ae6341bf935755f
juliotoscano <juliotoscanodebrito@gmail.com>
2017-02-20 20:37:31 -0300

    comment

--- musicard/settings.py
+++ musicard/settings.py
@@ -121,7 +121,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-
 STATIC_URL = '/static/'
 
 db_from_env = dj_database_url.config(conn_max_age=500)
