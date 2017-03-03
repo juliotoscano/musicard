a709f4d5714fa3dcf88c77ddc795c362d1b0187d
juliotoscano <juliotoscanodebrito@gmail.com>
2017-03-03 18:52:59 -0300

    admin models

--- core/admin.py
+++ core/admin.py
@@ -4,7 +4,15 @@ from .models import Music, Repertory
 
 # Register your models here.
 
-admin.site.register(Music)
-admin.site.register(Repertory)
+class MusicAdmin(admin.ModelAdmin):
+    list_display = ['name','slug','artist','tone']
+    search_fields = ['name','slug','artist','tone']
+
+class RepertoryAdmin(admin.ModelAdmin):
+    list_display = ['name','slug']
+    search_fields = ['name','slug']
+
+admin.site.register(Music, MusicAdmin)
+admin.site.register(Repertory, RepertoryAdmin)
 
 
